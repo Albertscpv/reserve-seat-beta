981054d7e77fb2fb3e996f4ac88b19bfb68cedbe
Albert Dev <125626643+Albertscpv@users.noreply.github.com>
2024-09-26 22:06:21 +0000

    SECURE KEYS AND INFO

--- reserve-it/firebaseConfig.ts
+++ reserve-it/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC94d_3d83kGfGIJuui6-li3BhxQr-0k1Y",
-  authDomain: "reserveit-754b2.firebaseapp.com",
-  projectId: "reserveit-754b2",
-  storageBucket: "reserveit-754b2.appspot.com",
-  messagingSenderId: "654867883625",
-  appId: "1:654867883625:web:5ab05903dcf6b31f3424f8",
-  measurementId: "G-JECTK7STG8"
+  apiKey:         process.env.API_KEY,
+  authDomain:     process.env.AUTH_DOMAIN ,
+  projectId:      process.env.PROJECT_ID,
+  storageBucket:  process.env.STORAGE_BUCKET ,
+  messagingSenderId:  process.env.MSMSENDER_ID ,
+  appId:          process.env. APP_ID,
+  measurementId:  process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
