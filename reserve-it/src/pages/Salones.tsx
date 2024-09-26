45ddd29ae2b67c9dd125392a3cbeec2b0097a09b
Albert Dev <125626643+Albertscpv@users.noreply.github.com>
2024-09-26 22:32:17 +0000

    update

--- reserve-it/src/pages/Salones.tsx
+++ reserve-it/src/pages/Salones.tsx
@@ -2,9 +2,28 @@ import { useState, useEffect } from "react";
 import { collection, addDoc, getDocs, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 import { db } from "/workspaces/reserve-seat-beta/reserve-it/firebaseConfig.js"
 
+interface Salon{
+    id:string;
+    name:string;
+    description?:string;
+    features:string[];
+    available:boolean;
+}
+
 function Salones (){
-    const [ salon, setSalon ] = useState('');
-    const [ salones, setSalones] = useState([]);
+    const [salones, setSalones] = useState<Salon[]>([]);
+    const [newSalon, setNewSalon] = useState({ name: '', description: '', features: '', available: false })
+
+
+    const fetchSalones = async () => {
+        const querySnapshot = await getDocs(collection(db, 'salones'))
+        const salonesArray: Salon[] = [];
+
+        querySnapshot.forEach((doc) => {
+            salonesArray.push({id: doc.id, ...doc.data()} as Salon);
+        });
+        setSalones(salonesArray)
+    }
 
  
     return(
