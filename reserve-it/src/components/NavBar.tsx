48e816362bcd3692d6cf1dd0c68cd8dd689688ea
Albert Dev <125626643+Albertscpv@users.noreply.github.com>
2024-10-01 16:59:57 +0000

    navbar adjustment

--- reserve-it/src/components/NavBar.tsx
+++ reserve-it/src/components/NavBar.tsx
@@ -11,15 +11,16 @@ const NavBar = () => {
     return(
         <>
         <div className="flex flex-row bg-white/40 p-6 mb-16">
-        <ul className="flex text-white gap-4">
+        <ul className="flex text-white gap-4 flex-grow">
             {navLinks.map((navLink => (
                 <li key={navLink.path}>
                     <Link to={navLink.path}>{navLink.title}</Link>
                 </li>                
             )))}
-            <Logout/>
         </ul>
-
+        <div>
+                <Logout/>
+            </div>
         </div>
         </>
     )
